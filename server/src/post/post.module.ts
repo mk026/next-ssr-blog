4c48171b9339ec0c8306f925566a2b1a4686f720
Mikhail <mk0602604@gmail.com>
2022-08-22 20:45:35 +0300

    Register controller and service in post module

--- server/src/post/post.module.ts
+++ server/src/post/post.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PostController } from './post.controller';
+import { PostService } from './post.service';
 
-@Module({})
+@Module({
+  controllers: [PostController],
+  providers: [PostService],
+})
 export class PostModule {}
