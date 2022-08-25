7400f24834a1ca5f4545498c01f688005f750a4f
Mikhail <mk0602604@gmail.com>
2022-08-25 22:40:30 +0300

    Register post/comment modules in app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, PostModule, CommentModule],
 })
 export class AppModule {}
