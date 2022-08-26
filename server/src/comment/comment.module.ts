c0de2897e3ef8ee61bb4b34bde221d78bda52d1b
Mikhail <mk0602604@gmail.com>
2022-08-26 23:38:38 +0300

    Register controller and service in comment module

--- server/src/comment/comment.module.ts
+++ server/src/comment/comment.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CommentController } from './comment.controller';
+import { CommentService } from './comment.service';
 
-@Module({})
+@Module({
+  controllers: [CommentController],
+  providers: [CommentService],
+})
 export class CommentModule {}
