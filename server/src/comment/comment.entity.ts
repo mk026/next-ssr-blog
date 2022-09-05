1978e3ceb71b73ec27c69fe905e9712bdc0a8d49
Mikhail <mk0602604@gmail.com>
2022-09-05 13:25:49 +0300

    Use typeorm decorators for entities

--- server/src/comment/comment.entity.ts
+++ server/src/comment/comment.entity.ts
@@ -1,4 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Comment {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   content: string;
 }
