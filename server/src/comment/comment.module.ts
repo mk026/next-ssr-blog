2fdbc20ef7502d915c2a27480d453039ca335107
Mikhail <mk0602604@gmail.com>
2022-09-05 13:28:20 +0300

    Load entities with typeorm forFeature module

--- server/src/comment/comment.module.ts
+++ server/src/comment/comment.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentController } from './comment.controller';
+import { Comment } from './comment.entity';
 import { CommentService } from './comment.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comment])],
   controllers: [CommentController],
   providers: [CommentService],
 })
