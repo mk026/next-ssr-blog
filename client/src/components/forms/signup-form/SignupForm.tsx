cb696f8104ed381491000ca5318a087be49a36e0
Mikhail <mk0602604@gmail.com>
2022-09-13 22:55:54 +0300

    Add signup-form component

---
+++ client/src/components/forms/signup-form/SignupForm.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+
+const SignupForm: FC = () => {
+  return (
+    <form>
+      <label>
+        Name
+        <input type="text" />
+      </label>
+      <label>
+        Email
+        <input type="text" />
+      </label>
+      <label>
+        Password
+        <input type="password" />
+      </label>
+    </form>
+  );
+};
+
+export default SignupForm;
