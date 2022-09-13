c6d5772586edff8343628f628237828946474779
Mikhail <mk0602604@gmail.com>
2022-09-13 22:48:48 +0300

    Add types for redux root state, store and dispatch

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -7,3 +7,7 @@ const rootReducer = combineReducers({
 });
 
 export const setupStore = () => configureStore({ reducer: rootReducer });
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
