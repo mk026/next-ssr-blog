970fb05f58743a322e085dd36b3db73c9ee86143
Mikhail <mk0602604@gmail.com>
2022-09-16 15:38:33 +0300

    Connect redux store

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,18 @@
-import "../styles/globals.scss";
+import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
+
 import Layout from "../components/layout/Layout";
+import store from "../store";
+
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -15,3 +15,7 @@ export const setupStore = () => configureStore({ reducer: rootReducer });
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore["dispatch"];
+
+const store = setupStore();
+
+export default store;
