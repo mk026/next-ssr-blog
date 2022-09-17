1d704085f683e413294b169d4bed730a765a5e93
Mikhail <mk0602604@gmail.com>
2022-09-18 00:04:41 +0300

    Add basic validation to add-post-form

--- client/src/components/forms/add-post-form/AddPostForm.tsx
+++ client/src/components/forms/add-post-form/AddPostForm.tsx
@@ -1,16 +1,31 @@
+import { Button } from "@mui/material";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 
 const AddPostForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
+
+  const addPostHandler = (values: any) => {
+    console.log(values);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(addPostHandler)}>
       <label>
         Title
-        <input type="text" />
+        <input type="text" {...register("title", { required: true })} />
       </label>
+      {errors.title && <p>Title is a required field</p>}
       <label>
         Content
-        <input type="text" />
+        <input type="text" {...register("content", { required: true })} />
       </label>
+      {errors.content && <p>Content is a required field</p>}
+      <Button type="submit">Add post</Button>
     </form>
   );
 };
