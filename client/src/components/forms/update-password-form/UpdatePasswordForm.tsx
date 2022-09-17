c63a9c4715a2452625d70dd8bc8b76d2e5169c9d
Mikhail <mk0602604@gmail.com>
2022-09-17 23:52:02 +0300

    Add basic validation to update-password-form

--- client/src/components/forms/update-password-form/UpdatePasswordForm.tsx
+++ client/src/components/forms/update-password-form/UpdatePasswordForm.tsx
@@ -1,20 +1,42 @@
+import { Button } from "@mui/material";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 
 const UpdatePasswordForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
+
+  const updatePasswordHandler = (values: any) => {
+    console.log(values);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(updatePasswordHandler)}>
       <label>
         Old password
-        <input type="password" />
+        <input
+          type="password"
+          {...register("oldPassword", { required: true })}
+        />
       </label>
+      {errors.oldPassword && <p>Old password is a required field</p>}
       <label>
         New password
-        <input type="password" />
+        <input type="password" {...register("password", { required: true })} />
       </label>
+      {errors.password && <p>New password is a required field</p>}
       <label>
         Confirm password
-        <input type="password" />
+        <input
+          type="password"
+          {...register("confirmPassword", { required: true })}
+        />
       </label>
+      {errors.confirmPassword && <p>Confirm password is a required field</p>}
+      <Button type="submit">Update password</Button>
     </form>
   );
 };
