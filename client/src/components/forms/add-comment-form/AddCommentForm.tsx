0bfbbc95e540e2caa112823798a48d8815be8beb
Mikhail <mk0602604@gmail.com>
2022-09-18 00:07:05 +0300

    Add basic validation to add-comment-form

--- client/src/components/forms/add-comment-form/AddCommentForm.tsx
+++ client/src/components/forms/add-comment-form/AddCommentForm.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@mui/material";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 
 const AddCommentForm: FC = () => {
+  const { register, handleSubmit } = useForm({ mode: "onBlur" });
+
+  const addCommentHandler = (values: any) => {
+    console.log(values);
+  };
+
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit(addCommentHandler)}>
+      <input type="text" {...register("content", { required: true })} />
+      <Button type="submit">Add comment</Button>
     </form>
   );
 };
