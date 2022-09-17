b467c508462d45a0d919135789f27bdb505e278e
Mikhail <mk0602604@gmail.com>
2022-09-17 23:59:14 +0300

    Add basic validation to update-profile-form

--- client/src/components/forms/update-profile-form/UpdateProfileForm.tsx
+++ client/src/components/forms/update-profile-form/UpdateProfileForm.tsx
@@ -1,16 +1,31 @@
+import { Button } from "@mui/material";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 
 const UpdateProfileForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
+
+  const updateProfileHandler = (values: any) => {
+    console.log(values);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(updateProfileHandler)}>
       <label>
         Name
-        <input type="text" />
+        <input type="text" {...register("name", { required: true })} />
       </label>
+      {errors.name && <p>Name is a required field</p>}
       <label>
         Email
-        <input type="text" />
+        <input type="text" {...register("email", { required: true })} />
       </label>
+      {errors.email && <p>Email is a required field</p>}
+      <Button type="submit">Update profile</Button>
     </form>
   );
 };
