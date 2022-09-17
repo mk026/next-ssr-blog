30dbaff93e18f6669353e591150dc57738be8594
Mikhail <mk0602604@gmail.com>
2022-09-17 23:48:34 +0300

    Add basic validation to signup-form

--- client/src/components/forms/signup-form/SignupForm.tsx
+++ client/src/components/forms/signup-form/SignupForm.tsx
@@ -1,20 +1,36 @@
+import { Button } from "@mui/material";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 
 const SignupForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
+
+  const signupHandler = (values: any) => {
+    console.log(values);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(signupHandler)}>
       <label>
         Name
-        <input type="text" />
+        <input type="text" {...register("name", { required: true })} />
       </label>
+      {errors.name && <p>Name is a required field</p>}
       <label>
         Email
-        <input type="text" />
+        <input type="text" {...register("email", { required: true })} />
       </label>
+      {errors.email && <p>Email is a required field</p>}
       <label>
         Password
-        <input type="password" />
+        <input type="password" {...register("password", { required: true })} />
       </label>
+      {errors.password && <p>Password is a required field</p>}
+      <Button type="submit">Signup</Button>
     </form>
   );
 };
