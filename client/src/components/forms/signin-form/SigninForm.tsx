d6772cfc7834d465d071f165e80098db89a549cd
Mikhail <mk0602604@gmail.com>
2022-09-18 22:26:03 +0300

    Add signin validation schema

--- client/src/components/forms/signin-form/SigninForm.tsx
+++ client/src/components/forms/signin-form/SigninForm.tsx
@@ -1,15 +1,24 @@
-import { Button } from "@mui/material";
 import { FC } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button } from "@mui/material";
+
+import {
+  SigninFormValues,
+  signinValidationSchema,
+} from "../../../validation/signinValidation";
 
 const SigninForm: FC = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: "onBlur" });
+  } = useForm<SigninFormValues>({
+    mode: "onBlur",
+    resolver: yupResolver(signinValidationSchema),
+  });
 
-  const signinHandler = (values: any) => {
+  const signinHandler = (values: SigninFormValues) => {
     console.log(values);
   };
 
@@ -17,14 +26,14 @@ const SigninForm: FC = () => {
     <form onSubmit={handleSubmit(signinHandler)}>
       <label>
         Email
-        <input type="text" {...register("email", { required: true })} />
+        <input type="text" {...register("email")} />
       </label>
-      {errors.email && <p>Email is a required field</p>}
+      {errors.email && <p>{errors.email.message}</p>}
       <label>
         Password
-        <input type="password" {...register("password", { required: true })} />
+        <input type="password" {...register("password")} />
       </label>
-      {errors.password && <p>Password is a required field</p>}
+      {errors.password && <p>{errors.password.message}</p>}
       <Button type="submit">Signin</Button>
     </form>
   );
