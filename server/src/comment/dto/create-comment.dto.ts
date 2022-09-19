0aed1459f3639abd6b559e10d754ab4c2e21db28
Mikhail <mk0602604@gmail.com>
2022-09-19 23:25:30 +0300

    Add string length validation for post/comment dto

--- server/src/comment/dto/create-comment.dto.ts
+++ server/src/comment/dto/create-comment.dto.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCommentDto {
   @IsNumber()
   readonly userId: number;
 
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(500)
   readonly content: string;
 }
--- server/src/post/dto/create-post.dto.ts
+++ server/src/post/dto/create-post.dto.ts
@@ -1,12 +1,16 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, MaxLength, MinLength } from 'class-validator';
 
 export class CreatePostDto {
   @IsNumber()
   readonly userId: number;
 
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(100)
   readonly title: string;
 
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(1000)
   readonly content: string;
 }
