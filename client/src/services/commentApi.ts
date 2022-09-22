9f757783f8581c19f415eb724ae1f05d7d29edb9
Mikhail <mk0602604@gmail.com>
2022-09-22 23:53:12 +0300

    Create addPostComment mutation in commentApi

--- client/src/services/commentApi.ts
+++ client/src/services/commentApi.ts
@@ -3,7 +3,11 @@ import { baseApi } from "./baseApi";
 export const commentApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getPostComments: builder.query({ query: () => "/comments" }),
+    addPostComment: builder.mutation({
+      query: () => ({ url: "/comments", method: "POST" }),
+    }),
   }),
 });
 
-export const { useGetPostCommentsQuery } = commentApi;
+export const { useGetPostCommentsQuery, useAddPostCommentMutation } =
+  commentApi;
