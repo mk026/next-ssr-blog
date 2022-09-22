58848aa59a16dccea38a45f999a539b7a1c72ca2
Mikhail <mk0602604@gmail.com>
2022-09-23 00:30:32 +0300

    Add body to signup/signin requests in authApi

--- client/src/services/authApi.ts
+++ client/src/services/authApi.ts
@@ -14,10 +14,10 @@ export const SIGNIN_URL = "/signin";
 export const authApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     signup: builder.mutation<AuthResponse, SignupFormValues>({
-      query: () => SIGNUP_URL,
+      query: (body) => ({ url: SIGNUP_URL, method: "POST", body }),
     }),
     signin: builder.mutation<AuthResponse, SigninFormValues>({
-      query: () => SIGNIN_URL,
+      query: (body) => ({ url: SIGNIN_URL, method: "POST", body }),
     }),
   }),
 });
