05f6cba20ea2ca416c5b859f96a1ceb29170ff6a
Mikhail <mk0602604@gmail.com>
2022-09-22 22:50:54 +0300

    Create addPost mutation in postApi

--- client/src/services/postApi.ts
+++ client/src/services/postApi.ts
@@ -3,7 +3,10 @@ import { baseApi } from "./baseApi";
 export const postApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getPosts: builder.query({ query: () => "/posts" }),
+    addPost: builder.mutation({
+      query: (body) => ({ url: "/posts", method: "POST", body }),
+    }),
   }),
 });
 
-export const { useGetPostsQuery } = postApi;
+export const { useGetPostsQuery, useAddPostMutation } = postApi;
