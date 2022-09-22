ea9602a05d865b7006a0b95c2331cc6513710e3e
Mikhail <mk0602604@gmail.com>
2022-09-23 00:32:47 +0300

    Create update/delete user mutations in userApi

--- client/src/services/userApi.ts
+++ client/src/services/userApi.ts
@@ -5,6 +5,12 @@ export const USERS_URL = "/users";
 export const userApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getUser: builder.query({ query: () => USERS_URL }),
+    updateUser: builder.mutation({
+      query: (body) => ({ url: USERS_URL, method: "PUT", body }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({ url: `${USERS_URL}/${id}`, method: "DELETE" }),
+    }),
   }),
 });
 
