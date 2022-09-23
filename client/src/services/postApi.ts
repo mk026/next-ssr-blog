a7801a33963a98222d60bee96db9b9aaa81a3b09
Mikhail <mk0602604@gmail.com>
2022-09-23 23:06:50 +0300

    Add types for postApi requests/responses

--- client/src/services/postApi.ts
+++ client/src/services/postApi.ts
@@ -1,17 +1,18 @@
+import { IPost } from "../models/IPost";
 import { baseApi } from "./baseApi";
 
 export const POSTS_URL = "/posts";
 
 export const postApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getPosts: builder.query({ query: () => POSTS_URL }),
-    addPost: builder.mutation({
+    getPosts: builder.query<IPost[], void>({ query: () => POSTS_URL }),
+    addPost: builder.mutation<IPost, IPost>({
       query: (body) => ({ url: POSTS_URL, method: "POST", body }),
     }),
-    updatePost: builder.mutation({
+    updatePost: builder.mutation<IPost, Partial<IPost>>({
       query: (body) => ({ url: POSTS_URL, method: "PUT", body }),
     }),
-    deletePost: builder.mutation({
+    deletePost: builder.mutation<void, string>({
       query: (id) => ({ url: `${POSTS_URL}/${id}`, method: "DELETE" }),
     }),
   }),
