1c91e74cfd2ecc8c70c7d293d4db07756768d366
Mikhail <mk0602604@gmail.com>
2022-09-23 23:04:20 +0300

    Add types for userApi requests/responses

--- client/src/services/userApi.ts
+++ client/src/services/userApi.ts
@@ -1,14 +1,17 @@
+import { IUser } from "../models/IUser";
 import { baseApi } from "./baseApi";
 
 export const USERS_URL = "/users";
 
 export const userApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getUser: builder.query({ query: () => USERS_URL }),
-    updateUser: builder.mutation({
+    getUser: builder.query<IUser, string>({
+      query: (id) => ({ url: `${USERS_URL}/${id}` }),
+    }),
+    updateUser: builder.mutation<IUser, Partial<IUser>>({
       query: (body) => ({ url: USERS_URL, method: "PUT", body }),
     }),
-    deleteUser: builder.mutation({
+    deleteUser: builder.mutation<void, string>({
       query: (id) => ({ url: `${USERS_URL}/${id}`, method: "DELETE" }),
     }),
   }),
