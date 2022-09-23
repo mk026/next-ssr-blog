bcc5a0c10e57d4d4d5fe629a4ad5a3001a647b4b
Mikhail <mk0602604@gmail.com>
2022-09-23 23:08:46 +0300

    Add types for commentApi requests/responses

--- client/src/services/commentApi.ts
+++ client/src/services/commentApi.ts
@@ -1,17 +1,20 @@
+import { IComment } from "../models/IComment";
 import { baseApi } from "./baseApi";
 
 export const COMMENTS_URL = "/comments";
 
 export const commentApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getPostComments: builder.query({ query: () => COMMENTS_URL }),
-    addPostComment: builder.mutation({
+    getPostComments: builder.query<IComment[], string>({
+      query: (postId) => ({ url: COMMENTS_URL, params: { postId } }),
+    }),
+    addPostComment: builder.mutation<IComment, IComment>({
       query: (body) => ({ url: COMMENTS_URL, method: "POST", body }),
     }),
-    updatePostComment: builder.mutation({
+    updatePostComment: builder.mutation<IComment, Partial<IComment>>({
       query: (body) => ({ url: COMMENTS_URL, method: "PUT", body }),
     }),
-    deletePostComment: builder.mutation({
+    deletePostComment: builder.mutation<void, string>({
       query: (id) => ({ url: `${COMMENTS_URL}/${id}`, method: "DELETE" }),
     }),
   }),
