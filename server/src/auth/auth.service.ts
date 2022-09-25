24760f3e0c4a49dfadc310f73c24d8dfeb8eb89f
Mikhail <mk0602604@gmail.com>
2022-09-25 19:05:41 +0300

    Add proper types for auth responses

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,19 +1,28 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import bcryptjs from 'bcryptjs';
+import { User } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
 import { SigninCredentialsDto } from './dto/signin-credentials.dto';
 import { SignupCredentialsDto } from './dto/signup-credentials.dto';
+
+export interface AuthResponse {
+  user: Pick<User, 'name' | 'email'>;
+}
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
 
-  async signup(signupCredentialsDto: SignupCredentialsDto) {
+  async signup(
+    signupCredentialsDto: SignupCredentialsDto,
+  ): Promise<AuthResponse> {
     const user = await this.userService.addUser(signupCredentialsDto);
     return { user: { name: user.name, email: user.email } };
   }
 
-  async signin(signinCredentialsDto: SigninCredentialsDto) {
+  async signin(
+    signinCredentialsDto: SigninCredentialsDto,
+  ): Promise<AuthResponse> {
     const { email, password } = signinCredentialsDto;
     const foundUser = await this.userService.getUserByEmail(email);
     if (!foundUser) {
