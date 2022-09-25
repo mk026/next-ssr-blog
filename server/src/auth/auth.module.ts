54103d0c8285b7dbcba78311d5b280d521ea4fa6
Mikhail <mk0602604@gmail.com>
2022-09-25 23:07:16 +0300

    Register passport and jwt modules

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
