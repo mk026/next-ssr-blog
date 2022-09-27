583051aa64cd89a1d8519b90a61f3ebb48db464a
Mikhail <mk0602604@gmail.com>
2022-09-27 22:55:29 +0300

    Fix get-user decorator

--- server/src/auth/get-user.decorator.ts
+++ server/src/auth/get-user.decorator.ts
@@ -1,3 +1,8 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const GetUser = createParamDecorator((data, req): number => req.user);
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): number => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
