c34ff29723d3f8eb57fd2aa7022f51e2f699aeb5
Mikhail <mk0602604@gmail.com>
2022-10-14 23:39:18 +0300

    Create posts-list component

---
+++ client/src/components/posts/posts-list/PostsList.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const PostsList: FC = () => {
+  return <div>PostsList</div>;
+};
+
+export default PostsList;
--- client/src/pages/posts/index.tsx
+++ client/src/pages/posts/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
+import { Container, Typography } from "@mui/material";
+import PostsList from "../../components/posts/posts-list/PostsList";
 
 const Posts: NextPage = () => {
-  return <div>Posts page</div>;
+  return (
+    <Container>
+      <Typography variant="h1"> Posts page</Typography>
+      <PostsList />
+    </Container>
+  );
 };
 
 export default Posts;
