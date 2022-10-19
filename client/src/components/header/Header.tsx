9771fe0b5a8ce2644e5240120fa8992691cb98d4
Mikhail <mk0602604@gmail.com>
2022-10-19 23:48:11 +0300

    Render signout button when user is signed in

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,18 +1,25 @@
 import { FC } from "react";
-import { Box, Stack } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 import Link from "next/link";
 
 import Navbar from "../navbar/Navbar";
+import { useAppSelector } from "../../hooks/redux";
+import { getAuthState } from "../../store/selectors/authSelectors";
 
 const Header: FC = () => {
+  const { isAuth } = useAppSelector(getAuthState);
+
   return (
     <Box component="header">
       <Stack direction="row">
         <Navbar />
-        <Box>
-          <Link href="/signup">Create Account</Link>
-          <Link href="/signin">Signin</Link>
-        </Box>
+        {!isAuth && (
+          <Box>
+            <Link href="/signup">Create Account</Link>
+            <Link href="/signin">Signin</Link>
+          </Box>
+        )}
+        {isAuth && <Button>Signout</Button>}
       </Stack>
     </Box>
   );
