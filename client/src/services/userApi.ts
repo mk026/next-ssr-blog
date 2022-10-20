cdc79dfd5d7d4c12058b14ed9a8885b1f4098451
Mikhail <mk0602604@gmail.com>
2022-10-20 23:13:29 +0300

    Create update-password mutation

--- client/src/services/userApi.ts
+++ client/src/services/userApi.ts
@@ -1,4 +1,5 @@
 import { IUser } from "../models/IUser";
+import { UpdatePasswordFormValues } from "../validation/updatePasswordValidation";
 import { baseApi, HttpMethod } from "./baseApi";
 
 export const USERS_URL = "/users";
@@ -11,10 +12,21 @@ export const userApi = baseApi.injectEndpoints({
     updateUser: builder.mutation<IUser, Partial<IUser>>({
       query: (body) => ({ url: USERS_URL, method: HttpMethod.PUT, body }),
     }),
+    updatePassword: builder.mutation<void, UpdatePasswordFormValues>({
+      query: (body) => ({
+        url: `${USERS_URL}/update_password`,
+        method: HttpMethod.PATCH,
+        body,
+      }),
+    }),
     deleteUser: builder.mutation<void, string>({
       query: (id) => ({ url: `${USERS_URL}/${id}`, method: HttpMethod.DELETE }),
     }),
   }),
 });
 
-export const { useGetUserQuery, useUpdateUserMutation } = userApi;
+export const {
+  useGetUserQuery,
+  useUpdateUserMutation,
+  useUpdatePasswordMutation,
+} = userApi;
