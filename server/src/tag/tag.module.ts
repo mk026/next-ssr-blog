3d4c3bc863759f580dbf4efc48118bdda3fa1082
Mikhail <mk0602604@gmail.com>
2022-12-04 22:51:32 +0300

    Register typeorm module for tag feature

--- server/src/tag/tag.module.ts
+++ server/src/tag/tag.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TagController } from './tag.controller';
+import { Tag } from './tag.entity';
 import { TagService } from './tag.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
   controllers: [TagController],
   providers: [TagService],
 })
