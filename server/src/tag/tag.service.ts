6a303396c544a36bb33920b3b73c8f3b21213bee
Mikhail <mk0602604@gmail.com>
2022-12-06 22:44:38 +0300

    Inject tag repository into tag service

--- server/src/tag/tag.service.ts
+++ server/src/tag/tag.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 import { CreateTagDto } from './dto/create-tag.dto';
+import { Tag } from './tag.entity';
 
 @Injectable()
 export class TagService {
+  constructor(
+    @InjectRepository(Tag) private readonly tagRepository: Repository<Tag>,
+  ) {}
+
   getTags() {
     return 'Get tags';
   }
