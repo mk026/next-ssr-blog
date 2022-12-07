bd774c87a0ce294b75627bf45d5e41f60bbcd03c
Mikhail <mk0602604@gmail.com>
2022-12-07 23:25:36 +0300

    Refactor markup for auth-links component

--- client/src/components/auth-links/AuthLinks.tsx
+++ client/src/components/auth-links/AuthLinks.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import Link from "next/link";
 
 import classes from "./AuthLinks.module.scss";
@@ -7,8 +7,12 @@ import classes from "./AuthLinks.module.scss";
 const AuthLinks: FC = () => {
   return (
     <Stack direction="row" className={classes.links}>
-      <Link href="/signup">Create Account</Link>
-      <Link href="/signin">Signin</Link>
+      <Button variant="outlined">
+        <Link href="/signup">Create Account</Link>
+      </Button>
+      <Button variant="contained">
+        <Link href="/signin">Signin</Link>
+      </Button>
     </Stack>
   );
 };
