34305c0dd77747de4691ede10bf36aa8bf2217e3
Mikhail <mk0602604@gmail.com>
2022-12-07 22:31:47 +0300

    Refactor comment api types
    Add required postId property for addComment req body type

--- client/src/store/api/commentApi.ts
+++ client/src/store/api/commentApi.ts
@@ -1,15 +1,19 @@
 import { IComment } from "../../models/IComment";
-import { CommentFormValues } from "../../validation/commentValidation";
 import { baseApi, HttpMethod } from "./baseApi";
 
 export const COMMENTS_URL = "/api/comments";
+
+export interface AddCommentDto {
+  postId: number;
+  content: string;
+}
 
 export const commentApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getPostComments: builder.query<IComment[], number>({
       query: (postId) => ({ url: COMMENTS_URL, params: { postId } }),
     }),
-    addPostComment: builder.mutation<IComment, CommentFormValues>({
+    addPostComment: builder.mutation<IComment, AddCommentDto>({
       query: (body) => ({ url: COMMENTS_URL, method: HttpMethod.POST, body }),
     }),
     updatePostComment: builder.mutation<IComment, Partial<IComment>>({
