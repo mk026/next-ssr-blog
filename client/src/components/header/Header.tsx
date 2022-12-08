3180afc2e7622b66df51a49760096095f49467b7
Mikhail <mk0602604@gmail.com>
2022-12-08 22:34:38 +0300

    Attach signout handler in header component

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -6,16 +6,21 @@ import Navbar from "../navbar/Navbar";
 import { useAppSelector } from "../../hooks/redux";
 import { getAuthState } from "../../store/selectors/authSelectors";
 import AuthLinks from "../auth-links/AuthLinks";
+import { useActions } from "../../hooks/useActions";
+import { authActions } from "../../store/slices/authSlice";
 
 const Header: FC = () => {
   const { isAuth } = useAppSelector(getAuthState);
+  const { signout } = useActions(authActions);
+
+  const signoutHandler = () => signout();
 
   return (
     <Box component="header">
       <Stack direction="row">
         <Navbar />
         {!isAuth && <AuthLinks />}
-        {isAuth && <Button>Signout</Button>}
+        {isAuth && <Button onClick={signoutHandler}>Signout</Button>}
       </Stack>
     </Box>
   );
