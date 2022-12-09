1e2e920362e3fa3368d403435a4683313ba7b761
Mikhail <mk0602604@gmail.com>
2022-12-09 21:48:36 +0300

    Add link to author's profile in comment-item

--- client/src/components/comments/comment-item/CommentItem.tsx
+++ client/src/components/comments/comment-item/CommentItem.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Card, Typography } from "@mui/material";
 import { FC } from "react";
+import { Avatar, Card, Typography } from "@mui/material";
+import Link from "next/link";
 
 import { IComment } from "../../../models/IComment";
 
@@ -11,7 +12,11 @@ const CommentItem: FC<CommentItemProps> = ({ comment }) => {
   return (
     <Card>
       <Avatar alt={comment.author.name} src={comment.author.avatarUrl} />
-      <Typography>{comment.author.name}</Typography>
+      <Typography>
+        <Link href={`/profile/${comment.author.id}`}>
+          {comment.author.name}
+        </Link>
+      </Typography>
       <Typography>{comment.content}</Typography>
     </Card>
   );
