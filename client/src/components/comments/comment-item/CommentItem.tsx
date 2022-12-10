7e32dc6ce83de7042e1bbe5e6e6fc0b67a7366cd
Mikhail <mk0602604@gmail.com>
2022-12-10 23:31:32 +0300

    Add styles for comment-item component

--- client/src/components/comments/comment-item/CommentItem.tsx
+++ client/src/components/comments/comment-item/CommentItem.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 
 import { IComment } from "../../../models/IComment";
 
+import classes from "./CommentItem.module.scss";
+
 interface CommentItemProps {
   comment: IComment;
 }
 
 const CommentItem: FC<CommentItemProps> = ({ comment }) => {
   return (
-    <Card>
+    <Card className={classes.comment}>
       <Avatar alt={comment.author.name} src={comment.author.avatarUrl} />
-      <Typography>
+      <Typography className={classes["author-name"]}>
         <Link href={`/profile/${comment.author.id}`}>
           {comment.author.name}
         </Link>
