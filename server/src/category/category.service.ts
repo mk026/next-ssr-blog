3ff81d1e649791ff45814be46aff96ccb4617327
Mikhail <mk0602604@gmail.com>
2022-12-11 22:45:06 +0300

    Inject category repository into category service

--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
+import { Category } from './category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
 export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
   getCategories() {
     return 'Get categories';
   }
