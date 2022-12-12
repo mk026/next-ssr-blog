1ed7ffe42cd6d01efef501d054ae7b5db2746335
Mikhail <mk0602604@gmail.com>
2022-12-12 23:00:15 +0300

    Add category and tags to post model

--- client/src/models/IPost.ts
+++ client/src/models/IPost.ts
@@ -1,8 +1,12 @@
 import { IUser } from "./IUser";
+import { ICategory } from "./ICategory";
+import { ITag } from "./ITag";
 
 export interface IPost {
   id: number;
   author: IUser;
   title: string;
   content: string;
+  category: ICategory;
+  tags: ITag[];
 }
