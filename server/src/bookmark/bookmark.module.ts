22be96463438cf5e6a5f498a93ef493f90aef63c
Mikhail <mk0602604@gmail.com>
2022-12-14 22:46:52 +0300

    Register typeorm module for bookmark feature

--- server/src/bookmark/bookmark.module.ts
+++ server/src/bookmark/bookmark.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BookmarkController } from './bookmark.controller';
+import { Bookmark } from './bookmark.entity';
 import { BookmarkService } from './bookmark.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Bookmark])],
   controllers: [BookmarkController],
   providers: [BookmarkService],
 })
