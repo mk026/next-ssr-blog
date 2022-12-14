277c45bf7c34fdbcfedc9c66285a0ccde375fcb8
Mikhail <mk0602604@gmail.com>
2022-12-14 23:05:29 +0300

    Inject bookmark repository into bookmark service

--- server/src/bookmark/bookmark.service.ts
+++ server/src/bookmark/bookmark.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Bookmark } from './bookmark.entity';
 import { CreateBookmarkDto } from './dto/create-bookmark.dto';
 
 @Injectable()
 export class BookmarkService {
+  constructor(
+    @InjectRepository(Bookmark)
+    private readonly bookmarkRepository: Repository<Bookmark>,
+  ) {}
+
   getBookmarks(userId: number) {
     return `Get bookmarks for user with id ${userId}`;
   }
