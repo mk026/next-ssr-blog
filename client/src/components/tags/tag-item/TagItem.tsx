4c1c536b5afd2ef1f846838cdd2187613c326e1e
Mikhail <mk0602604@gmail.com>
2022-12-17 23:52:51 +0300

    Create tags-list/tag-item components

---
+++ client/src/components/tags/tag-item/TagItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TagItem = () => {
+  return <div>TagItem</div>;
+};
+
+export default TagItem;
---
+++ client/src/components/tags/tags-list/TagsList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TagsList = () => {
+  return <div>TagsList</div>;
+};
+
+export default TagsList;
