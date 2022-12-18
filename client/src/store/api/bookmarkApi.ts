2fccf986275c8bee14ce65740a09fe27b2f04a88
Mikhail <mk0602604@gmail.com>
2022-12-18 23:20:58 +0300

    Inject add/delete bookmark endpoints

--- client/src/store/api/bookmarkApi.ts
+++ client/src/store/api/bookmarkApi.ts
@@ -1,4 +1,4 @@
-import { baseApi } from "./baseApi";
+import { baseApi, HttpMethod } from "./baseApi";
 
 export const BOOKMARKS_URL = "/api/bookmarks";
 
@@ -6,6 +6,15 @@ export const bookmarkApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getBookmarks: builder.query<any, void>({
       query: () => BOOKMARKS_URL,
+    }),
+    addBookmark: builder.mutation<any, any>({
+      query: (body) => ({ url: BOOKMARKS_URL, method: HttpMethod.POST, body }),
+    }),
+    removeBookmark: builder.mutation<any, number>({
+      query: (id) => ({
+        url: `${BOOKMARKS_URL}/${id}`,
+        method: HttpMethod.DELETE,
+      }),
     }),
   }),
 });
