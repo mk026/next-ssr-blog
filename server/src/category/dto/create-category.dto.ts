62894212fe6571e54490eb3bd3160f45758a634a
Mikhail <mk0602604@gmail.com>
2022-12-21 23:44:47 +0300

    Validate title length in category/tag dto's

--- server/src/category/dto/create-category.dto.ts
+++ server/src/category/dto/create-category.dto.ts
@@ -1,6 +1,8 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(50)
   readonly title: string;
 }
--- server/src/tag/dto/create-tag.dto.ts
+++ server/src/tag/dto/create-tag.dto.ts
@@ -1,6 +1,8 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateTagDto {
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(50)
   readonly title: string;
 }
