3e4338eb4b0fac0f469cf7d73e59cc2276af288e
Mikhail <mk0602604@gmail.com>
2022-12-23 21:30:36 +0300

    Setup swagger

---
+++ server/src/swagger/index.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('NextJS Blog Platform API')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};
