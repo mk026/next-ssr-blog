4f982e78bcafe463eb7575fcb8abdc45b46d6aa4
Mikhail <mk0602604@gmail.com>
2022-12-27 22:24:01 +0300

    Use form-field in search-post-form

--- client/src/components/forms/search-post-form/SearchPostForm.tsx
+++ client/src/components/forms/search-post-form/SearchPostForm.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
-import { useForm } from "react-hook-form";
-import { Box, Button, TextField } from "@mui/material";
+import { FormProvider, useForm } from "react-hook-form";
+import { Box, Button } from "@mui/material";
+
+import FormField from "../../form-field/FormField";
 
 export interface SearchPostFormValues {
   query: string;
 }
 
 const SearchPostForm: FC = () => {
-  const { register, handleSubmit } = useForm<SearchPostFormValues>({
+  const methods = useForm<SearchPostFormValues>({
     mode: "onBlur",
   });
 
@@ -16,10 +18,12 @@ const SearchPostForm: FC = () => {
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit(searchPostHandler)}>
-      <TextField label="Search post" {...register("query")} />
-      <Button type="submit">Search</Button>
-    </Box>
+    <FormProvider {...methods}>
+      <Box component="form" onSubmit={methods.handleSubmit(searchPostHandler)}>
+        <FormField label="Search post" name="query" />
+        <Button type="submit">Search</Button>
+      </Box>
+    </FormProvider>
   );
 };
 
