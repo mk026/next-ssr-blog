31579d1402fa480179beaa266bca4e76d40bba19
Mikhail <mk0602604@gmail.com>
2022-12-27 23:18:29 +0300

    Add interface for addBookmark request body

--- client/src/store/api/bookmarkApi.ts
+++ client/src/store/api/bookmarkApi.ts
@@ -2,12 +2,16 @@ import { baseApi, HttpMethod } from "./baseApi";
 
 export const BOOKMARKS_URL = "/api/bookmarks";
 
+export interface AddBookmarkDto {
+  postId: number;
+}
+
 export const bookmarkApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getBookmarks: builder.query<any, void>({
       query: () => BOOKMARKS_URL,
     }),
-    addBookmark: builder.mutation<any, any>({
+    addBookmark: builder.mutation<any, AddBookmarkDto>({
       query: (body) => ({ url: BOOKMARKS_URL, method: HttpMethod.POST, body }),
     }),
     removeBookmark: builder.mutation<any, number>({
