9d3dfaaac61093d93d2d0b9cfef417dbca54df16
Mikhail <mk0602604@gmail.com>
2022-12-27 22:52:27 +0300

    Refactor userApi interfaces
    Add correct interfaces for update-user/password request bodies

--- client/src/store/api/userApi.ts
+++ client/src/store/api/userApi.ts
@@ -1,19 +1,30 @@
 import { IUser } from "../../models/IUser";
-import { UpdatePasswordFormValues } from "../../validation/updatePasswordValidation";
 import { baseApi, HttpMethod } from "./baseApi";
 
 export const USERS_URL = "/users";
 export const PASSWORD_UPDATE_URL = "/users/password";
+
+export interface UpdateUserDto {
+  name?: string;
+  email?: string;
+  avatarUrl?: string;
+}
+
+export interface UpdatePasswordDto {
+  oldPassword: string;
+  password: string;
+  confirmPassword: string;
+}
 
 export const userApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getUser: builder.query<IUser, string>({
       query: (id) => ({ url: `${USERS_URL}/${id}` }),
     }),
-    updateUser: builder.mutation<IUser, Partial<IUser>>({
+    updateUser: builder.mutation<IUser, UpdateUserDto>({
       query: (body) => ({ url: USERS_URL, method: HttpMethod.PUT, body }),
     }),
-    updatePassword: builder.mutation<void, UpdatePasswordFormValues>({
+    updatePassword: builder.mutation<void, UpdatePasswordDto>({
       query: (body) => ({
         url: PASSWORD_UPDATE_URL,
         method: HttpMethod.PATCH,
