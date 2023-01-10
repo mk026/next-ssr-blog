50d553d951b5984556c93e35b43ea0b7bd71bd05
Mikhail <mk0602604@gmail.com>
2023-01-10 22:39:17 +0300

    Refactor update-profile validation schema
    Add bio prop, make all props optional

--- client/src/validation/updateProfileValidation.ts
+++ client/src/validation/updateProfileValidation.ts
@@ -3,11 +3,13 @@ import * as yup from "yup";
 import { userRules } from "./rules";
 
 export interface UpdateProfileFormValues {
-  name: string;
-  email: string;
+  name?: string;
+  bio?: string;
+  email?: string;
 }
 
 export const updateProfileValidationSchema = yup.object({
-  name: yup.string().min(userRules.name.min).max(userRules.name.max).required(),
-  email: yup.string().email().required(),
+  name: yup.string().min(userRules.name.min).max(userRules.name.max).optional(),
+  bio: yup.string().min(userRules.bio.min).max(userRules.bio.max).optional(),
+  email: yup.string().email().optional(),
 });
