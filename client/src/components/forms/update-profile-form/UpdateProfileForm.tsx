503f9fc0c427fd8e1ceb55b036ad7605b7a4acd8
Mikhail <mk0602604@gmail.com>
2023-01-10 22:40:17 +0300

    Add bio field in update-profile-form

--- client/src/components/forms/update-profile-form/UpdateProfileForm.tsx
+++ client/src/components/forms/update-profile-form/UpdateProfileForm.tsx
@@ -29,6 +29,7 @@ const UpdateProfileForm: FC = () => {
         onSubmit={methods.handleSubmit(updateProfileHandler)}
       >
         <FormField label="Name" name="name" />
+        <FormField label="About yourself" name="bio" multiline />
         <FormField label="Email" name="email" />
         <LoadingButton isLoading={isLoading}>Update profile</LoadingButton>
       </Box>
