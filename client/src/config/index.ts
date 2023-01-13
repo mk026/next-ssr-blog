bb01553954beb7ffa8fa43ed0aee812124312ea7
Mikhail <mk0602604@gmail.com>
2023-01-13 23:37:15 +0300

    Create config file

---
+++ client/src/config/index.ts
@@ -0,0 +1,14 @@
+export const config = {
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL!,
+  signupUrl: process.env.NEXT_PUBLIC_API_SIGNUP_URL!,
+  signinUrl: process.env.NEXT_PUBLIC_API_SIGNIN_URL!,
+  usersUrl: process.env.NEXT_PUBLIC_API_USERS_URL!,
+  passwordUpdateUrl: process.env.NEXT_PUBLIC_API_PASSWORD_UPDATE_URL!,
+  postsUrl: process.env.NEXT_PUBLIC_API_POSTS_URL!,
+  popularPostsUrl: process.env.NEXT_PUBLIC_API_POPULAR_POSTS_URL!,
+  searchPostsUrl: process.env.NEXT_PUBLIC_API_SEARCH_POSTS_URL!,
+  commentsUrl: process.env.NEXT_PUBLIC_API_COMMENTS_URL!,
+  bookmarksUrl: process.env.NEXT_PUBLIC_API_BOOKMARKS_URL!,
+  categoriesUrl: process.env.NEXT_PUBLIC_API_CATEGORIES_URL!,
+  tagsUrl: process.env.NEXT_PUBLIC_API_TAGS_URL!,
+};
