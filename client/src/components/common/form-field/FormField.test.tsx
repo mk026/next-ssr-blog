226a77fa273a1e5f842d632bce158ff47f4b03b0
Mikhail <mk0602604@gmail.com>
2023-01-17 01:35:50 +0300

    Fix form-field test
    Create wrapper for form-field to provide form context

--- client/src/components/common/form-field/FormField.test.tsx
+++ client/src/components/common/form-field/FormField.test.tsx
@@ -1,3 +1,5 @@
+import { FC, PropsWithChildren } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { render, screen } from "@testing-library/react";
 
 import FormField from ".";
@@ -6,9 +8,19 @@ it("Should render FormField", () => {
   const fieldName = "Field Name";
   const fieldLabel = "Field Label";
 
-  render(<FormField name={fieldName} label={fieldLabel} />);
+  const Wrapper: FC<PropsWithChildren> = ({ children }) => {
+    const methods = useForm();
 
-  const element = screen.getByText(fieldLabel);
+    return <FormProvider {...methods}>{children}</FormProvider>;
+  };
+
+  render(
+    <Wrapper>
+      <FormField name={fieldName} label={fieldLabel} />
+    </Wrapper>
+  );
+
+  const element = screen.getByText(fieldLabel, { ignore: "span" });
 
   expect(element).toBeInTheDocument();
 });
