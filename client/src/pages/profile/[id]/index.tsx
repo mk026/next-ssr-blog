6775a3b4a26a9a4bdd87c604fee0efc840050032
Mikhail <mk0602604@gmail.com>
2023-01-28 23:41:51 +0300

    Use get-user query in dynamic profile page

--- client/src/pages/profile/[id]/index.tsx
+++ client/src/pages/profile/[id]/index.tsx
@@ -2,13 +2,21 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Typography } from "@mui/material";
 
+import { useGetUserQuery } from "../../../store/api/userApi";
+import { useRouter } from "next/router";
+
 const Profile: NextPage = () => {
+  const router = useRouter();
+  const userId = router.query.id as string;
+  const { data: user } = useGetUserQuery(userId);
+
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>{user?.name || "User Profile"}</title>
       </Head>
-      <Typography variant="h1">Profile page</Typography>
+      <Typography variant="h1">{user?.name}</Typography>
+      <Typography variant="body1">{user?.bio}</Typography>
     </>
   );
 };
