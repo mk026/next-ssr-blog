fc195396830584f68ce41ff12ba00128d5c0ba28
Mikhail <mk0602604@gmail.com>
2023-01-30 22:29:51 +0300

    Add test for comment-item component

---
+++ client/src/components/comments/comment-item/CommentItem.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+
+import CommentItem from ".";
+import { IComment } from "../../../models/IComment";
+import { IUser } from "../../../models/IUser";
+import { setupStore } from "../../../store";
+
+it("Should render CommentItem", () => {
+  const fakeAuthor: IUser = {
+    id: 1,
+    avatarUrl: "fake_avatar_url",
+    bio: "Fake User Bio",
+    createdAt: new Date().toLocaleString(),
+    email: "fake_user_email",
+    name: "Fake User",
+    updatedAt: new Date().toLocaleString(),
+  };
+  const fakeComment: IComment = {
+    id: 1,
+    author: fakeAuthor,
+    content: "Fake Comment Text",
+    createdAt: new Date().toLocaleString(),
+    postId: 1,
+    updatedAt: new Date().toLocaleString(),
+  };
+  const store = setupStore();
+
+  render(
+    <Provider store={store}>
+      <CommentItem comment={fakeComment} />
+    </Provider>
+  );
+
+  const element = screen.getByText(fakeComment.content);
+
+  expect(element).toBeInTheDocument();
+});
