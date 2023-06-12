a0dfabf1e2cad1ea4dc4dff5e809bf26114dc846
Mikhail <mk0602604@gmail.com>
2023-06-12 23:36:23 +0300

    Refactor header component
    Add state to control user-menu component visibility

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,23 +1,28 @@
-import { FC } from "react";
-import { AppBar, Box, Stack } from "@mui/material";
+import { FC, useState } from "react";
+import { AppBar, Stack } from "@mui/material";
 
 import { useAppSelector } from "../../hooks/redux";
 import { getAuthState } from "../../store/selectors/authSelectors";
 import Navbar from "../navbar";
-import SignoutButton from "../signout-button";
 import AuthLinks from "../auth-links";
+import UserMenuButton from "../user-menu-button";
+import UserMenu from "../user-menu";
 
 import classes from "./Header.module.scss";
 
 const Header: FC = () => {
   const { isAuth } = useAppSelector(getAuthState);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const toggleMenu = () => setMenuIsOpen((prev) => !prev);
 
   return (
     <AppBar data-testid="header" className={classes.header}>
       <Stack direction="row">
         <Navbar />
-        {!isAuth ? <AuthLinks /> : <SignoutButton />}
+        {!isAuth ? <AuthLinks /> : <UserMenuButton onClick={toggleMenu} />}
       </Stack>
+      {menuIsOpen && <UserMenu />}
     </AppBar>
   );
 };
