8dc9886cf9e0f2a44ca1aa4168479c7fa1b696d1
Mikhail <mk0602604@gmail.com>
2023-06-13 23:32:20 +0300

    Refactor user-menu component

--- client/src/components/user-menu/UserMenu.tsx
+++ client/src/components/user-menu/UserMenu.tsx
@@ -1,13 +1,18 @@
-import { Stack, Typography } from "@mui/material";
 import { FC } from "react";
+import { Button, Stack, Typography } from "@mui/material";
+
+import { useActions } from "../../hooks/useActions";
+import { authActions } from "../../store/slices/authSlice";
 
 const UserMenu: FC = () => {
+  const { signout } = useActions(authActions);
+
   return (
     <Stack>
       <Typography>Profile</Typography>
       <Typography>Posts</Typography>
       <Typography>Bookmarks</Typography>
-      <Typography>Signout</Typography>
+      <Button onClick={signout}>Signout</Button>
     </Stack>
   );
 };
