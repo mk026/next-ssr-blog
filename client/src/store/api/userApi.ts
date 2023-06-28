08021c5e449e8ae97d958cd4e616a3a12d2fd362
Mikhail <mk0602604@gmail.com>
2023-06-28 22:19:58 +0300

    Inject search-users endpoint in user-api

--- client/src/store/api/userApi.ts
+++ client/src/store/api/userApi.ts
@@ -7,6 +7,9 @@ export const userApi = baseApi.injectEndpoints({
     getUser: builder.query<IUser, string>({
       query: (id) => ({ url: `${config.usersUrl}/${id}` }),
     }),
+    searchUsers: builder.query<IUser, void>({
+      query: () => ({ url: config.searchUsersUrl }),
+    }),
     updateUser: builder.mutation<IUser, UpdateUserDto>({
       query: (body) => ({ url: config.usersUrl, method: HttpMethod.PUT, body }),
     }),
