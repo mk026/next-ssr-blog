84df1a644ef30a4955f19dd21cc5d4eacd443c6a
Mikhail <mk0602604@gmail.com>
2023-06-28 22:37:12 +0300

    Create search-user-form component

---
+++ client/src/components/forms/search-user-form/SearchUserForm.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+const SearchUserForm: FC = () => {
+  return (
+    <>
+      <form>
+        <label>
+          User Name
+          <input type="text" />
+        </label>
+      </form>
+    </>
+  );
+};
+
+export default SearchUserForm;
