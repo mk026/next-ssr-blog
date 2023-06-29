f4ed548004f3fefc9b46f75e1d3ecefd4faf2931
Mikhail <mk0602604@gmail.com>
2023-06-29 23:03:28 +0300

    Refactor home page markup

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { Typography } from "@mui/material";
 import type { NextPage } from "next";
 
 import CustomHead from "../components/common/custom-head";
+import PageTitle from "../components/common/page-title";
 
 const Home: NextPage = () => {
   return (
     <>
       <CustomHead title="Next Blog" />
-      <Typography variant="h1">Next Blog</Typography>
+      <PageTitle>Next Blog</PageTitle>
     </>
   );
 };
