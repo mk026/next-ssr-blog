8e898cbc174b549132e960bd55024affed807c06
Mikhail <mk0602604@gmail.com>
2023-07-01 22:37:34 +0300

    Render search-users query result on search page

--- client/src/pages/profile/search/index.tsx
+++ client/src/pages/profile/search/index.tsx
@@ -1,15 +1,25 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
+import { useSearchUsersQuery } from "../../../store/api/userApi";
 import CustomHead from "../../../components/common/custom-head";
 import PageTitle from "../../../components/common/page-title";
 import SearchUserForm from "../../../components/forms/search-user-form";
+import UserProfilesList from "../../../components/user-profiles/user-profiles-list";
 
 const SearchProfile: NextPage = () => {
+  const router = useRouter();
+  const { data, isLoading } = useSearchUsersQuery({
+    name: router.query.name as string,
+  });
+
   return (
     <>
       <CustomHead title="Search Profile" />
       <PageTitle>Search Profile</PageTitle>
       <SearchUserForm />
+      {isLoading && <p>Loading...</p>}
+      <UserProfilesList users={data || []} />
     </>
   );
 };
