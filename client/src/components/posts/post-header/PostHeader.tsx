0bfea9edaa36bba0b234cb110adb355aaddef86b
Mikhail <mk0602604@gmail.com>
2023-07-07 22:17:08 +0300

    Add styles for post-header component

--- client/src/components/posts/post-header/PostHeader.tsx
+++ client/src/components/posts/post-header/PostHeader.tsx
@@ -3,14 +3,18 @@ import { Avatar, Box, Stack, Typography } from "@mui/material";
 
 import { IPost } from "../../../models/post";
 
+import classes from "./PostHeader.module.scss";
+
 interface PostHeaderProps {
   post: IPost;
 }
 
 const PostHeader: FC<PostHeaderProps> = ({ post }) => {
   return (
-    <Stack direction="row">
-      <Typography variant="h2">{post.title}</Typography>
+    <Stack direction="row" className={classes.header}>
+      <Typography variant="h2" className={classes.title}>
+        {post.title}
+      </Typography>
       <Box>
         <Typography>{post.author.name}</Typography>
         <Avatar alt={post.author.name} src={post.author.avatarUrl} />
