962a21b3f4a193e256d27b309042bc39c6d32751
Mikhail <mk0602604@gmail.com>
2023-07-07 22:16:27 +0300

    Add styles for add-post-link component

--- client/src/components/posts/add-post-link/AddPostLink.tsx
+++ client/src/components/posts/add-post-link/AddPostLink.tsx
@@ -2,9 +2,11 @@ import { FC } from "react";
 import Link from "next/link";
 import { Button } from "@mui/material";
 
+import classes from "./AddPostLink.module.scss";
+
 const AddPostLink: FC = () => {
   return (
-    <Link href="/posts/add">
+    <Link href="/posts/add" className={classes.link}>
       <Button>Add new post</Button>
     </Link>
   );
